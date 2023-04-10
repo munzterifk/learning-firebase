727fce848fca40fa166e990fd42514e7fd0b6918
noel <noelpastranacastor@gmail.com>
2023-04-10 18:31:09 +0800

    test commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="title-row">
-        <h1 className="title">Firebase Recipes</h1>
+        <h1 className="title">Learning firebase</h1>
       </div>
     </div>
   );
